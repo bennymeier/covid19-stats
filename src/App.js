d96421ea41ffef4bce72f5ddd9751d5b103cf771
Benny <meier.benjamin@web.de>
2020-10-24 00:27:07 +0200

    List all countries

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [overall, setOverall] = useState(null);
+  useEffect(async () => {
+    const data = await fetch(
+      "https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/2020-10-22/2020-10-22"
+    );
+    const jsonData = await data.json();
+    console.log(jsonData);
+    if (jsonData.countries) {
+      setCountries(jsonData.countries);
+    }
+    if (jsonData.scale) {
+      setOverall(jsonData.scale);
+    }
+  }, []);
   return (
     <div className="App">
-      <div>Hello!</div>
+      <h1>Covid 19 Stats</h1>
+      <div>
+        <div>
+          <ul>
+            {countries.map((country) => {
+              return <li key={country}>{country}</li>;
+            })}
+          </ul>
+        </div>
+        <div>
+          <ul></ul>
+        </div>
+      </div>
     </div>
   );
 }
